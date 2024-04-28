fab2bd27af3e1cbde1cab92542de8661ef05c875
Matteo <S296340@studenti.polito.it>
2024-04-28 22:19:02 +0200

    finished version, needs uddate

--- UI/controller.py
+++ UI/controller.py
@@ -1,5 +1,6 @@
-import flet as ft
+import copy
 
+import flet as ft
 from UI.view import View
 from model.model import Model
 
@@ -12,15 +13,64 @@ class Controller:
         self._model = model
         # other attributes
         self._mese = 0
+        self._giorni=14
+        self._sequenzaOttima=[]
 
     def handle_umidita_media(self, e):
-        pass
+        dict_umidita=self._model.getUmidita(self._mese)
+        self._view.lst_result.controls.append(ft.Text(f"L'umidità nel mese selezionato è:"))
+        for citta in dict_umidita:
+            tot=0
+            for situazione in dict_umidita[citta]:
+               tot+=situazione.umidita
+            media=tot/len(dict_umidita[citta])
+            self._view.lst_result.controls.append(ft.Text(f"{citta}: {round(media,4)}"))
+        self._view.update_page()
 
 
 
     def handle_sequenza(self, e):
-        pass
-
+        dict_umidita=self._model.getUmiditaGiorni(self._mese)
+        giorni=0
+        list_umidita=[]
+        for giorno in dict_umidita:
+            list_umidita.append(dict_umidita[giorno])
+        print("Inizio ricorsione")
+        self._ricorsione(list_umidita,giorni,[],[],0)
+        self._sequenzaOttima.sort(key=lambda x: x[1])
+        sol=self._sequenzaOttima[0]
+        self._view.lst_result.controls.append(ft.Text(f"La sequenza ottima ha costo {sol[1]} ed è"))
+        for situazione in sol[0]:
+            self._view.lst_result.controls.append(ft.Text(f"[{situazione.localita} - {situazione.data}] Umidità={situazione.umidita}"))
+        self._view.update_page()
     def read_mese(self, e):
         self._mese = int(e.control.value)
 
+    def _ricorsione(self, list_situazioni,giorno,parziale,req,costo):
+        if giorno==self._giorni:
+            self._sequenzaOttima.append((copy.deepcopy(parziale),costo))
+        else:
+            for situazione in list_situazioni[giorno]:
+                parziale.append(situazione)
+                req.append(situazione.localita)
+                if self._situazione_ammissibile(parziale,req,situazione):
+                    #if giorno >= 1:
+                        #if req[giorno] == req[giorno - 1]:
+                            costo += situazione.umidita
+                            self._ricorsione(list_situazioni, giorno + 1, parziale, req, costo)
+                        #else:
+                         #   costo += (100 + situazione.umidita)
+                          #  self._ricorsione(list_situazioni, giorno + 1, parziale, req, costo)
+                    #else:
+                    #    costo += situazione.umidita
+                     #   self._ricorsione(list_situazioni, giorno + 1, parziale, req, costo)
+                req.pop()
+                parziale.pop()
+
+    def _situazione_ammissibile(self, parziale, req, situazione):
+        if len(parziale)>3:
+            if req[-1]==req[-2]==req[-3]==req[-4]:
+                return False
+            if req.count(situazione.localita)>6:
+                return False
+        return True
--- model/model.py
+++ model/model.py
@@ -1,3 +1,25 @@
+from database.meteo_dao import MeteoDao
 class Model:
     def __init__(self):
-        pass
+        self._meteo_dao=MeteoDao()
+
+    def getUmidita(self,mese):
+        self.list_situazioni=self._meteo_dao.get_all_situazioni()
+        result={}
+        for situazione in self.list_situazioni:
+            if situazione.data.month==mese:
+                if situazione.localita in result:
+                    result[situazione.localita].append(situazione)
+                else:
+                    result[situazione.localita]=[situazione]
+        return result
+    def getUmiditaGiorni(self,mese):
+        self.list_situazioni=self._meteo_dao.get_all_situazioni()
+        result={}
+        for situazione in self.list_situazioni:
+            if situazione.data.month==mese:
+                if situazione.data.day in result:
+                    result[situazione.data.day].append(situazione)
+                else:
+                    result[situazione.data.day]=[situazione]
+        return result
